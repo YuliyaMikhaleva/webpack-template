e42ef390c13a39a7eb11f91289f8a1176a614d59
YuliyaMikhaleva <umihaleva93@mail.ru>
2021-12-28 14:46:21 +0500

    Внесены изменения в slider.js для чтения изображений в слайдере

--- src/js/slider.js
+++ src/js/slider.js
@@ -9,7 +9,8 @@ const sliders = document.querySelectorAll('.swiper');
 console.log(sliders)
 if (sliders.length !== 0){
   sliders.forEach((slider) => {
-    const mySwiper = new Swiper('.swiper-container', {
+    const container = slider.querySelector('.swiper-container');
+    const mySwiper = new Swiper(container, {
       loop: true,
       slidesPerView: 1,
       spaceBetween: 20,
@@ -25,32 +26,20 @@ if (sliders.length !== 0){
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      on:{
-        slideChange:function () {
-          console.log(slider)//выдает все слайдеры, а не только тот, который листали
-      },
-
-    }});
-
-    // mySwiper.on('slideChange', function () {
-    //   console.log(slider)
-    // })
-    //событие изменения слайда
-    // mySwiper.on('slideChange', function () => {
-    //   const images = slider.querySelectorAll('.slider__img');//все картинки слайдера
-    //   console.log('Картинки этого слайдера:', images)
-    //   const array = slider.querySelectorAll('.swiper-pagination-bullet');//все кнопки пагинации
-    //
-    //   const activeBlock = slider.querySelector('.swiper-pagination-bullet-active');//активная кнопка
-    //   let el = activeBlock.getAttribute('aria-label').slice(-1);
-    //   let activeNumber = el - 1;//порядковый номер
-    //   images.forEach((image, index) => {
-    //     if (image.classList.contains('active-img')){
-    //       image.classList.remove('active-img');
-    //     };
-    //   })
-    //   images[activeNumber].classList.add('active-img')
-    // })
+});
+    // событие изменения слайда
+    mySwiper.on('slideChange', () => {
+      const images = slider.querySelectorAll('.slider__img');//все картинки слайдера
+      const activeBlock = slider.querySelector('.swiper-pagination-bullet-active');//активная кнопка
+      let el = activeBlock.getAttribute('aria-label').slice(-1);
+      let activeNumber = el - 1;//порядковый номер
+      images.forEach((image, index) => {
+        if (image.classList.contains('active-img')){
+          image.classList.remove('active-img');
+        };
+      })
+      images[activeNumber].classList.add('active-img')
+    })
 
   })
 
