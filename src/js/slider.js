b9d0af4a822dd01af0c0df86f3a293ad421a92e0
YuliyaMikhaleva <umihaleva93@mail.ru>
2021-12-28 11:43:29 +0500

    Перевёрстан блок slider, изображения пока не меняются

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,40 +2,57 @@ import Swiper, {Navigation, Pagination} from 'swiper';
 
 Swiper.use([Navigation, Pagination]);
 
-const slider = document.querySelector('.swiper-container')
 
-const mySwiper = new Swiper(slider, {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 20,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-})
+const sliders = document.querySelectorAll('.swiper');
+console.log(sliders)
+if (sliders.length !== 0){
+  sliders.forEach((slider) => {
+    const mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 20,
 
-// export default new Swiper('.swiper-container', {
-//   spaceBetween: 20,
-//   slidesPerView: 3,
-//   loop: true,
-//
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on:{
+        slideChange:function () {
+          console.log(slider)//выдает все слайдеры, а не только тот, который листали
+      },
+
+    }});
+
+    // mySwiper.on('slideChange', function () {
+    //   console.log(slider)
+    // })
+    //событие изменения слайда
+    // mySwiper.on('slideChange', function () => {
+    //   const images = slider.querySelectorAll('.slider__img');//все картинки слайдера
+    //   console.log('Картинки этого слайдера:', images)
+    //   const array = slider.querySelectorAll('.swiper-pagination-bullet');//все кнопки пагинации
+    //
+    //   const activeBlock = slider.querySelector('.swiper-pagination-bullet-active');//активная кнопка
+    //   let el = activeBlock.getAttribute('aria-label').slice(-1);
+    //   let activeNumber = el - 1;//порядковый номер
+    //   images.forEach((image, index) => {
+    //     if (image.classList.contains('active-img')){
+    //       image.classList.remove('active-img');
+    //     };
+    //   })
+    //   images[activeNumber].classList.add('active-img')
+    // })
+
+  })
+
+}
+
